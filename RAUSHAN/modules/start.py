ea8dca4d4c744e2c199060cebb196aa332d5031c
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-12-28 15:40:33 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- RAUSHAN/modules/start.py
+++ RAUSHAN/modules/start.py
@@ -22,37 +22,46 @@ from RAUSHAN.Helpers.mongo import (
 @app.on_message(filters.command("start") & filters.private)
 async def start_cmd(client, message: Message):
     user = message.from_user
-    user_id = user.id
-    username = f"@{user.username}" if user.username else "N/A"
-    mention = user.mention
     bot = (await client.get_me()).mention
 
     try:
-        await add_served_user(user_id)
+        await add_served_user(user.id)
 
-        await message.reply_photo(
-            photo=START_PIC,
-            caption=PM_TEXT.format(mention, bot),
-            reply_markup=PM_BUTTON,
-        )
+        
+        try:
+            await message.reply_photo(
+                photo=START_PIC,
+                caption=PM_TEXT.format(user.mention, bot),
+                reply_markup=PM_BUTTON,
+            )
+        except Exception as e:
+        
+            print("START PHOTO ERROR:", e)
+            await message.reply_text(
+                PM_TEXT.format(user.mention, bot),
+                reply_markup=PM_BUTTON,
+            )
 
     except (UserIsBlocked, InputUserDeactivated):
-        await remove_served_user(user_id)
+        await remove_served_user(user.id)
         return
-    except Exception:
+    except Exception as e:
+        print("START CMD ERROR:", e)
         return
 
+    
     log_msg = (
-        "**✦ ηєᴡ ᴜsєʀ sᴛᴧʀᴛєᴅ ᴛʜє ʙσᴛ**\n\n"
-        f"**➻ ᴜsєʀ :** [{user.first_name}](tg://user?id={user_id})\n"
-        f"**➻ ᴜsєʀɴᴀᴍᴇ :** {username}\n"
-        f"**➻ ɪᴅ :** `{user_id}`"
+        "**✦ ηєᴡ ᴜsєʀ sᴛᴀʀᴛєᴅ ᴛʜє ʙσᴛ**\n\n"
+        f"**➻ ᴜsєʀ :** [{user.first_name}](tg://user?id={user.id})\n"
+        f"**➻ ᴜsєʀɴᴀᴍᴇ :** @{user.username if user.username else 'N/A'}\n"
+        f"**➻ ɪᴅ :** `{user.id}`"
     )
 
     try:
         await client.send_message(Config.LOGGER_GROUP_ID, log_msg)
     except:
         pass
+
 
 
 @app.on_message(filters.command("hack") & filters.private)
@@ -64,8 +73,8 @@ async def hack_cmd(client, message: Message):
         )
     except (UserIsBlocked, InputUserDeactivated):
         await remove_served_user(message.from_user.id)
-    except Exception:
-        pass
+    except Exception as e:
+        print("HACK CMD ERROR:", e)
 
 
 @app.on_callback_query(filters.regex("^hack_btn$"))
@@ -76,35 +85,38 @@ async def hack_callback(client, query: CallbackQuery):
             reply_markup=ALPHA_MODS,
         )
         await query.answer()
-    except Exception:
-        pass
+    except Exception as e:
+        print("HACK CALLBACK ERROR:", e)
 
 
 @app.on_callback_query(filters.regex("^back_btn$"))
 async def back_callback(client, query: CallbackQuery):
+    user = query.from_user
+    bot = (await client.get_me()).mention
+
     try:
         await query.message.edit_text(
-            PM_TEXT,
+            PM_TEXT.format(user.mention, bot),
             reply_markup=PM_BUTTON,
         )
         await query.answer()
-    except Exception:
-        pass
+    except Exception as e:
+        print("BACK CALLBACK ERROR:", e)
+
 
 
 @app.on_message(filters.new_chat_members)
 async def on_bot_added(client, message: Message):
     bot_id = (await client.get_me()).id
 
-    if bot_id not in [user.id for user in message.new_chat_members]:
+    if bot_id not in [u.id for u in message.new_chat_members]:
         return
 
-    chat_id = message.chat.id
-
     try:
-        await add_served_chat(chat_id)
-    except:
-        pass
+        await add_served_chat(message.chat.id)
+    except Exception as e:
+        print("ADD CHAT ERROR:", e)
+
 
 
 @app.on_message(filters.left_chat_member)
@@ -114,9 +126,7 @@ async def on_bot_removed(client, message: Message):
     if message.left_chat_member.id != bot_id:
         return
 
-    chat_id = message.chat.id
-
     try:
-        await remove_served_chat(chat_id)
-    except:
-        pass
+        await remove_served_chat(message.chat.id)
+    except Exception as e:
+        print("REMOVE CHAT ERROR:", e)
