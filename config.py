e213de26c09a44886f0233f65c7606a5b18aa4f9
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-12-28 14:36:52 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 # TEAM PURVI ALL COPYRIGHT ©️
-from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "27079591"
-    API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
-    TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
-    SUDOERS = filters.user(["7473021518"])
+    API_ID = int(os.getenv("API_ID", "27079591"))
+    API_HASH = os.getenv("API_HASH", "c81ae4c3dc026ea4bf49842a8ce4a5f9")
+    TOKEN = os.getenv("TOKEN", None)
+    MONGO_URL = os.getenv("MONGO_URL", None)
+    OWNER_ID = int(os.getenv("OWNER_ID", "7473021518"))
+    START_PIC = os.getenv("START_PIC", "https://files.catbox.moe/ppvvg0.jpg")
