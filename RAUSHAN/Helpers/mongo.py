aa03079e0d9afa1a69405ecfb871a4c7fffac233
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-12-28 16:40:25 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- RAUSHAN/Helpers/mongo.py
+++ RAUSHAN/Helpers/mongo.py
@@ -2,10 +2,9 @@ import sys
 from config import Config
 from motor.motor_asyncio import AsyncIOMotorClient
 
-
 try:
-    mongodb = AsyncIOMotorClient(Config.MONGO_URL).Alpha
-except Exception as e:
+    mongodb = AsyncIOMotorClient(Config.MONGO_URL).ALPHA
+except Exception:
     print("Please change your MongoDB URL")
     sys.exit()
 
@@ -13,46 +12,37 @@ chatsdb = mongodb.tgchatsdb
 usersdb = mongodb.tgusersdb
 
 
-
 async def is_served_user(user_id: int) -> bool:
-    user = await usersdb.find_one({"user_id": user_id})
-    return bool(user)
+    return bool(await usersdb.find_one({"user_id": user_id}))
 
 
 async def get_served_users() -> list:
-    users_list = []
-    async for user in usersdb.find({"user_id": {"$gt": 0}}):
-        users_list.append(user)
-    return users_list
+    return [user async for user in usersdb.find({"user_id": {"$gt": 0}})]
 
 
 async def add_served_user(user_id: int):
     if await is_served_user(user_id):
         return
-    return await usersdb.insert_one({"user_id": user_id})
+    await usersdb.insert_one({"user_id": user_id})
 
 
 async def remove_served_user(user_id: int):
-    return await usersdb.delete_one({"user_id": user_id})
+    await usersdb.delete_one({"user_id": user_id})
 
 
 async def get_served_chats() -> list:
-    chats_list = []
-    async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
-        chats_list.append(chat)
-    return chats_list
+    return [chat async for chat in chatsdb.find({"chat_id": {"$lt": 0}})]
 
 
 async def is_served_chat(chat_id: int) -> bool:
-    chat = await chatsdb.find_one({"chat_id": chat_id})
-    return bool(chat)
+    return bool(await chatsdb.find_one({"chat_id": chat_id}))
 
 
 async def add_served_chat(chat_id: int):
     if await is_served_chat(chat_id):
         return
-    return await chatsdb.insert_one({"chat_id": chat_id})
+    await chatsdb.insert_one({"chat_id": chat_id})
 
 
 async def remove_served_chat(chat_id: int):
-    return await chatsdb.delete_one({"chat_id": chat_id})
+    await chatsdb.delete_one({"chat_id": chat_id})
