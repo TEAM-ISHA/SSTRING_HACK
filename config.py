9b6172ebb5db082fbd0630a1f6864fd60c17e62b
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-12-28 14:50:09 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     TOKEN = os.getenv("TOKEN", None)
     MONGO_URL = os.getenv("MONGO_URL", None)
     OWNER_ID = int(os.getenv("OWNER_ID", "7473021518"))
-    LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1003055975764"))
+    LOGGER_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1003055975764"))
     START_PIC = os.getenv("START_PIC", "https://files.catbox.moe/ppvvg0.jpg")
